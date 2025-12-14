206c4c711201413c38e923eb72fb1cac0b0a6c9c
Paul1723 <balanpaul.2003@yahoo.com>
2025-12-14 19:01:49 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import pandas as pd
+from pymongo import MongoClient
+from datetime import datetime
+
+connection_string = "mongodb+srv://BALAN_PAUL:stud@mongo.crqvzcq.mongodb.net"
+
+try:
+    client = MongoClient(connection_string)
+    db = client["StreamFlex"]
+    collection = db["divertisment"]
+except Exception as e:
+    st.error(f"Connection Error: {e}")
+    st.stop()
+
+st.set_page_config(page_title="StreamFlex Manager", layout="wide")
+st.title("StreamFlex Admin Panel")
+
+st.subheader("Search and Filter")
+
+with st.form("search_form"):
+    c1, c2 = st.columns(2)
+    with c1:
+        filter_type = st.selectbox("Content Type", ["All", "Film", "Serial"])
+    with c2:
+        search_title = st.text_input("Title (or part of it)")
+    
+    c3, c4 = st.columns(2)
+    with c3:
+        search_genre = st.text_input("Genre (e.g. Drama, Sci-Fi)")
+    with c4:
+        # Slider for Rating
+        min_rating = st.slider("Minimum Rating", 0.0, 10.0, 0.0, step=0.1)
+
+    st.write("")
+    search_submitted = st.form_submit_button("Apply Filters")
+
+query = {}
+
+
+if filter_type != "All":
+    query["tip"] = filter_type
+
+if search_title:
+    query["titlu"] = {"$regex": search_title, "$options": "i"}
+
+if search_genre:
+    query["genuri"] = {"$regex": search_genre, "$options": "i"}
+
+if min_rating > 0:
+    query["nota"] = {"$gte": min_rating}
+
+items = list(collection.find(query, {"_id": 0, "titlu": 1, "tip": 1, "nota": 1, "genuri": 1, "recomandat": 1}))
+
+st.divider()
+col_res, col_space = st.columns([1, 5])
+col_res.metric("Results Found", len(items))
+
+if items:
+    df = pd.DataFrame(items)
+    cols = [c for c in ['titlu', 'tip', 'nota', 'recomandat', 'genuri'] if c in df.columns]
+    
+    dynamic_height = (len(df) * 35) + 38
+
+    st.dataframe(
+        df[cols], 
+        use_container_width=True, 
+        height=dynamic_height,  
+        hide_index=True
+    )
+else:
+    st.warning("No results found matching your filters.")
+
+
+st.divider()
+st.subheader("Add New Title")
+
+with st.form("add_form", clear_on_submit=True):
+    c1, c2, c3, c4 = st.columns(4)
+    new_title = c1.text_input("Title")
+    new_type = c2.selectbox("Type", ["Film", "Serial"])
+    new_rating = c3.number_input("Rating", 0.0, 10.0, step=0.1)
+    new_genre = c4.text_input("Genre")
+    
+    save_submitted = st.form_submit_button("Save to Database")
+    
+    if save_submitted and new_title:
+        new_doc = {
+            "titlu": new_title,
+            "tip": new_type,
+            "nota": new_rating,
+            "genuri": [new_genre],
+            "data_lansare": datetime.now(),
+            "recomandat": True if new_rating >= 8.5 else False
+        }
+        collection.insert_one(new_doc)
+        st.success(f"Success: {new_title} added.")
+        st.rerun()
